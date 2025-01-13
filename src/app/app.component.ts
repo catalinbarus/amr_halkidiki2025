52816d7d4fe5d760877efbde96eb10a2fa417770
Catalin Barus <catalinstefanb@gmail.com>
2025-01-13 21:23:27 +0200

    Made amr build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amr_halkidiki2025';
+
+  currentDate = Date.now();
+
+  calculateDaysToJuly10(): number {
+    const today = new Date(); // Current date
+    const year = today.getMonth() > 6 || (today.getMonth() === 6 && today.getDate() > 10)
+      ? today.getFullYear() + 1 // Next year's July 10 if today is past July 10
+      : today.getFullYear();
+
+    const targetDate = new Date(year, 6, 10); // July 10th (Month is 0-based)
+    const diffInTime = targetDate.getTime() - today.getTime();
+    const diffInDays = Math.ceil(diffInTime / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
+
+    return diffInDays;
+  }
+
 }
